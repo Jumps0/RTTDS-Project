df8fda1e2413addc00a155a3387f218c29baf9f7
Jumps0 <49619518+Jumps0@users.noreply.github.com>
2024-10-21 18:12:49 +0200

    Acceleration for movement

--- RTTDS/Assets/Resources/Scripts/Actors/Entity.cs
+++ RTTDS/Assets/Resources/Scripts/Actors/Entity.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Rendering;
 using UnityEngine;
 
 /// <summary>
@@ -9,10 +10,15 @@ public class Entity : MonoBehaviour
     public Rigidbody2D rb;
 
     [Header("Values")]
+    [Header("- Movement")]
     private Vector2 movementVector;
     public float speed_max = 50f;
     public float speed_rotation = 150f;
     public float speed_rotationTurret = 150f;
+    public float acceleration = 70f;
+    public float deceleration = 50f;
+    public float speed_current = 0;
+    public float currentForwardDirection = 1;
     //
     public string uniqueName;
     // ...
@@ -34,9 +40,34 @@ public class Entity : MonoBehaviour
         Debug.Log($"{this.gameObject.name} is shooting.");
     }
 
-    public void HandleMoveBody(Vector2 movementVector)
+    public void Move(Vector2 movementVector)
     {
         this.movementVector = movementVector;
+
+        CalculateSpeed(movementVector);
+
+        if(movementVector.y > 0)
+        {
+            currentForwardDirection = 1;
+        }
+        else if (movementVector.y < 0)
+        {
+            currentForwardDirection = 0;
+        }
+
+    }
+
+    private void CalculateSpeed(Vector2 movementVector)
+    {
+        if(Mathf.Abs(movementVector.y) > 0)
+        {
+            speed_current += acceleration * Time.deltaTime;
+        }
+        else
+        {
+            speed_current -= deceleration * Time.deltaTime;
+        }
+        speed_current = Mathf.Clamp(speed_current, 0, speed_max);
     }
 
     public void HandleTurretMovement(Vector2 pointerPosition)
@@ -53,7 +84,7 @@ public class Entity : MonoBehaviour
     public void FixedUpdate()
     {
         // Velocity
-        rb.linearVelocity = (Vector2)transform.up * movementVector.y * speed_max * Time.fixedDeltaTime;
+        rb.linearVelocity = (Vector2)transform.up * speed_current * currentForwardDirection * Time.fixedDeltaTime;
         // Rotation
         rb.MoveRotation(transform.rotation * Quaternion.Euler(0, 0, -movementVector.x * speed_rotation * Time.fixedDeltaTime));
     }
